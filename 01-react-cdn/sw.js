9a7f540094e06e32d013cc4dd52455d3dccc2d4b
diegofdg <diegofdg@gmail.com>
2022-07-21 15:37:04 -0300

    01-react-cdn - install sw

--- 01-react-cdn/sw.js
+++ 01-react-cdn/sw.js
@@ -1,1 +1,23 @@
-console.log("registrado");
+const CACHE_ELEMENTS = [
+    "./",
+    "https://unpkg.com/react@18/umd/react.production.min.js",
+    "https://unpkg.com/react-dom@18/umd/react-dom.production.min.js",
+    "https://unpkg.com/@babel/standalone/babel.min.js"
+]
+
+const CACHE_NAME = "v1_cache_react_cdn"
+
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                cache.addAll(CACHE_ELEMENTS)
+                    .then(
+                        self.skipWaiting()
+                    )
+                    .catch(
+                        error => console.log(error)
+                    )
+            })
+    )
+})
