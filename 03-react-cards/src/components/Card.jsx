34a80ec2eb6ebedb8e871d18adfdf75c3bd1d733
diegofdg <diegofdg@gmail.com>
2022-07-23 09:06:56 -0300

    03-react-cards - proptypes

--- 03-react-cards/src/components/Card.jsx
+++ 03-react-cards/src/components/Card.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 import './Card.css'
 
-const Card = ({ lang, img, fcolor, scolor }) => {
-    return (
-      <div className="card">
+const Card = ( {lang, img, fcolor, scolor }) => {
+  return (
+    <div className="card">
         <img src={img} alt="lang.svg" />
         <h3>{lang}</h3>
       </div>
-    );
-  };
+  )
+}
+
+Card.propTypes = {
+    lang: PropTypes.string,
+    img: PropTypes.string,
+    fcolor: PropTypes.string,
+    scolor: PropTypes.string,
+}
 
 export default Card
