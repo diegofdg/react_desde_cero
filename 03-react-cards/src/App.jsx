31fe9e3f9de18036db0e25324523f8b00158b766
diegofdg <diegofdg@gmail.com>
2022-07-23 08:40:55 -0300

    03-react-cards - fragment

--- 03-react-cards/src/App.jsx
+++ 03-react-cards/src/App.jsx
@@ -1,8 +1,8 @@
-import Card from './components/Card'
+import Cards from './components/Cards'
 
 const App = () => {
   return (
-    <Card />
+    <Cards />
   )
 }
 
---
+++ 03-react-cards/src/components/Cards.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+import Card from './Card'
+
+const Cards = () => {
+  return (
+    <Fragment>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+    </Fragment>    
+  )
+}
+
+export default Cards
