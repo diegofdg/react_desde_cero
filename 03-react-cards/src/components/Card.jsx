2dc624f6ddf7e0286a3b6f7bdf5993b0cd11f5dd
diegofdg <diegofdg@gmail.com>
2022-07-23 08:24:44 -0300

    03-react-cards - estructurando una plantilla

---
+++ 03-react-cards/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+      <div>
+        <img src="#" alt="lang.svg" />
+        <h3>Lenguaje</h3>
+      </div>
+    );
+  };
+
+export default Card
