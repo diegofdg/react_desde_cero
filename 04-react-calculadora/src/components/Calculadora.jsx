b4886054fb062da8e515dc1c5f8d5cf1b943342a
diegofdg <diegofdg@gmail.com>
2022-08-07 18:27:39 -0300

    04-react-calculadora - analizando el componente

--- 04-react-calculadora/src/components/Calculadora.jsx
+++ 04-react-calculadora/src/components/Calculadora.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Calculadora = () => {
-  return (
-    <div>
-      <label>
-        Numero 1: <input type="text" />        
-      </label>
-    </div>
-  )
+    const [suma, setSuma] = useState(0)
+    return (
+        <div>
+            <label>
+                Numero 1: <input type="text" />        
+            </label>
+            <label>
+                Numero 2: <input type="text" />        
+            </label>
+            <br />
+
+            <span>Suma: {suma}</span>
+            <br />
+
+            <span>Resta: {suma}</span>
+            <br />
+
+            <span>Multiplicacion: {suma}</span>
+            <br />
+
+            <span>Division: {suma}</span>
+        </div>
+    )
 }
 
 export default Calculadora
