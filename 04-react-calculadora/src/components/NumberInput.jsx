22dbfb07a5e4e58f63b746e6a3ae6913983dd5d2
diegofdg <diegofdg@gmail.com>
2022-08-07 18:58:14 -0300

    04-react-calculadora - convirtiendo tipos de datos

--- 04-react-calculadora/src/components/NumberInput.jsx
+++ 04-react-calculadora/src/components/NumberInput.jsx
@@ -3,19 +3,16 @@ import { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const NumberInput = ({ name }) => {
-    const [numeros, setNumeros] = useState({
-        numero1: 0,
-        numero2: 0
-    })
+    const [numeros, setNumeros] = useState(0)
 
     const handleChange = (e) => {
-        setNumeros(e.target.value)
+        setNumeros(parseFloat(e.target.value))
     }
 
     return (
         <>
             <label>
-                {name}: <input onChange={handleChange} type="text" />
+                {name}: <input value={numeros} onChange={handleChange} type="number" />
             </label>
         </>
     )
