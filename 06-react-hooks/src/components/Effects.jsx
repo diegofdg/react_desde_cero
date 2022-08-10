c473b45fbc17d7e807eccdb9aac2ace58c66b171
diegofdg <diegofdg@gmail.com>
2022-08-10 16:10:56 -0300

    06-react-hooks - dependency

--- 06-react-hooks/src/components/Effects.jsx
+++ 06-react-hooks/src/components/Effects.jsx
@@ -4,24 +4,26 @@ import { useState } from "react"
 const Effects = () => {
 
     const [users, setUsers] = useState([])
+    const [id, setId] = useState(10)
 
-    useEffect(async () => {
-        const res = await fetch("https://jsonplaceholder.typicode.com/users")
-        const data = await res.json()
-        setUsers(data)
-    }, [])      
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((data) => setUsers(data))        
+    }, [])
     
-    console.log(users)
+    useEffect(() => {
+        setUsers({
+            hola: "cambien en el id"
+        })
+        console.log(users)
+    }, [id])
 
     const handleSubmit = (e) => {
         e.preventDefault()
-
-        console.log({
-            name: "Nombre",
-            email: "Email",
-            password: "Password"
-        })
+        setId(e.target.value)
     }
+
     return (
         <>
             <h1>useEffect</h1>
@@ -31,7 +33,9 @@ const Effects = () => {
                     <label htmlFor="exampleInputEmail1" className="form-label">
                         Search
                     </label>
-                    <input                
+                    <input 
+                        value={id}
+                        onChange={(e) => setId(e.target.value)}             
                         type="text"
                         className="form-control"
                         id="exampleInputEmail1"
