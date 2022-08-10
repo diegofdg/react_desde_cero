dc8181d8e4413083c93bf20baf4b2011327db1d4
diegofdg <diegofdg@gmail.com>
2022-08-10 07:19:58 -0300

    06-react-hooks - useEffect-hook

--- 06-react-hooks/src/components/State.jsx
+++ 06-react-hooks/src/components/State.jsx
@@ -1,18 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const State = () => {
 
   const [state, setState ] = useState(0)
 
-  setInterval(()=> {
-    setState(state+1)
-  },3000)
+  useEffect(()=> {
+    console.log(state)
+  }, [])
   
+
+  const handleClick = () => {
+    setState(state +1)
+  }
+
   return (
     <div className='container text-center'>
-        <h1>useState</h1>
+        <h1>useEffect</h1>
         <hr />
         Cuenta: {state}
+        <button onClick={handleClick}>+1</button>
     </div>
   )
 }
