77f9e91bb857e1d9bf3803e1df5b4e16787f287b
diegofdg <diegofdg@gmail.com>
2022-08-10 20:56:45 -0300

    06-react-hooks - useMemo-hook

--- 06-react-hooks/src/components/memos/Memorize.jsx
+++ 06-react-hooks/src/components/memos/Memorize.jsx
@@ -1,9 +1,18 @@
-import React, {useState } from 'react';
-import Dato from "./Dato";
+import React, {useState, useMemo } from 'react'
+import Dato from "./Dato"
 
 const Memorize = () => {
-  const [counter, setCounter] = useState(5);
+  const [counter, setCounter] = useState(5)
   const [view, setView] = useState(true)
+
+  const pesado = (iter) => {
+    for(let i = 0; i < iter; i++) {
+        console.log('procesando')
+    }
+    return 'Fin del proceso'
+  }
+
+  const pesadoMemo = useMemo(()=> pesado(counter), [counter])
 
   const handleClick = () => {
     setCounter(counter + 1)
@@ -15,11 +24,13 @@ const Memorize = () => {
         Memorize: <Dato value={counter} />
       </h1>
       <hr />
+
+      {pesadoMemo}
       
       <button onClick={handleClick}>+1</button>
       <button onClick={() => setView(!view)}>Ver/Quitar</button>
     </>
-  );
-};
+  )
+}
 
-export default Memorize;
+export default Memorize
